67afa18a28f27a422170f0bc4638fe14e81c240e
HITLADBALVANTRAI <101315912+HITLADBALVANTRAI@users.noreply.github.com>
2022-03-30 20:57:13 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.penup()
+turtle.setposition(0,0)
+turtle.pendown()
+turtle.pencolor("red")
+turtle.circle(100)
+turtle.penup()
+turtle.setposition(-200,200)
+turtle.pendown()
+turtle.pencolor("green")
+turtle.circle(200)
